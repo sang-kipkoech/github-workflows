f4e49e21185133a92d63c47227ea802dcec491dd
sang-kipkoech <kipkoech@untapped-global.com>
2023-04-14 11:59:18 +0300

    feat: cache example

---
+++ main.py
@@ -0,0 +1,24 @@
+import collections
+
+class CachingLRU:
+
+    def __init__(self, capacity) -> None:
+        self.capacity = capacity
+        self.cache = collections.OrderedDict()
+
+    def get(self, key):
+        try:
+            value = self.cache.pop(key)
+            self.cache[key] = value
+            return value
+        except KeyError:
+            return -1
+    
+    def set(self, key, value):
+        try:
+            self.cache.pop(key)
+        except KeyError:
+            if len(self.cache) >= self.capacity:
+                self.cache.popitem(last=False)
+                self.cache[key] = value
+
